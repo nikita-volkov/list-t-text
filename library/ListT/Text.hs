fd77855997f9af1b027e47aeb26c297a8f93e124
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-04-11 16:43:41 +0300

    Init

---
+++ library/ListT/Text.hs
@@ -0,0 +1,27 @@
+module ListT.Text where
+
+import BasePrelude hiding (cons, uncons)
+import MTLPrelude
+import ListT
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text as T
+import qualified Data.ByteString as B
+
+
+utf8ByteString :: Transformation m ByteString Text
+utf8ByteString =
+  loop TE.streamDecodeUtf8
+  where
+    loop decode stream =
+      lift (uncons stream) >>= \case
+        Nothing -> mzero
+        Just (chunk, stream') -> 
+          decode chunk & \(TE.Some result leftover decode') ->
+            if
+              | not (T.null result) -> cons result (loop decode' stream')
+              | not (B.null leftover) -> loop decode' stream'
+              | otherwise -> mzero
+              
+
