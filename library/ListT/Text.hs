bd4bb0e38f2f6c2274297a0b24fe7730fb2a21a0
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-04-28 20:16:21 +0300

    "stream" and renaming

--- library/ListT/Text.hs
+++ library/ListT/Text.hs
@@ -13,8 +13,8 @@ import qualified Data.ByteString as B
 -- |
 -- A transformation of a stream of byte-strings 
 -- to a stream of text chunks decoded using UTF-8.
-utf8ByteString :: Monad m => Transformation m ByteString Text
-utf8ByteString =
+decodeUTF8 :: Monad m => Transformation m ByteString Text
+decodeUTF8 =
   loop TE.streamDecodeUtf8
   where
     loop decode stream =
@@ -24,3 +24,11 @@ utf8ByteString =
           decode chunk & \(TE.Some result leftover decode') ->
             bool (cons result) id (T.null result) (loop decode' stream')
 
+-- |
+-- Stream text in chunks of the specified size.
+stream :: Monad m => Int -> Text -> ListT m Text
+stream n b =
+  T.splitAt n b & \(l, r) -> 
+  if T.null l
+    then mzero
+    else cons l (stream n r)
